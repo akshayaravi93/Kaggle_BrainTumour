8e9246c39f0c9baafa8f9007cf00970640a92fcc
Akshaya Ravi <akshayaarul@gmail.com>
2021-12-06 23:59:41 +0530

    back to early version

--- train.py
+++ train.py
@@ -248,7 +248,7 @@ def train():
 
     data_loader = data.DataLoader(dataset, args.batch_size,
                                   num_workers=args.num_workers,
-                                  shuffle=False, collate_fn=detection_collate,
+                                  shuffle=True, collate_fn=detection_collate,
                                   pin_memory=True)
     
     
